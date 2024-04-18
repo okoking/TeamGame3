d1389ff84074aa6d1249a5aa13d9fcd5924f6d8b
RoswelHelkaiser <144640067+RoswelHelkaiser@users.noreply.github.com>
2024-04-18 11:18:47 +0900

    Create Title

--- src/Scene/Title/Title.cpp
+++ src/Scene/Title/Title.cpp
@@ -21,12 +21,23 @@ void Title::Init()
 	//bgm
 	Sound::Bgm::Play(BGM_TITLE);
 	Sound::Bgm::SetVolume(BGM_TITLE,50);
-	//歯車回転
-	gearAngle = 0.0f;
 
 	//透過点滅
 	fade[0] = 0;
 	fade[1] = 0;
+
+	//明るさ
+	bright = 0;
+
+	//タイトルの初期Y座標を設定
+	for (int i = 0; i < 2; i++)
+	{
+		titlePosY[i] = -150.0f;
+		//到達フラグを折る
+		isReach[i] = false;
+	}
+
+	FrameCount = 0;
 
 	//通常処理に移行
 	g_CurrentSceneID = SCENE_ID_LOOP_TITLE;
@@ -38,126 +49,181 @@ void Title::Init()
 //通常処理
 void Title::Step()
 {
-	//現れる
-	if (lighting)
+	//到達フラグがfalseなら
+	if (!isReach[0])
 	{
-		fade[0] += 4;
-		fade[1] += 7;
+		titlePosY[0]++;
 
-		if (fade[0] >= 255)
+		if (titlePosY[0] >= 75.0f)
 		{
-			lighting = false;
-		}
-		if (fade[1] >= 255)
-		{
-			lighting = false;
-		}
-	}
-	//消える
-	else
-	{
-		fade[0] -= 4;
-		fade[1] -= 7;
-
-		if (fade[0] <= 0)
-		{
-			lighting = true;
-		}
-		if (fade[1] <= 0)
-		{
-			lighting = true;
+			titlePosY[0] = 75.0f;
+			isReach[0] = true;
 		}
 	}
 
-	gearAngle+=0.1f;
-		
-
-	//スペースキーで画面変化
-	if (Input::Key::Push(KEY_INPUT_SPACE))
+	else if (isReach[0] && !isReach[1])
 	{
-		//se
-		Sound::Se::Play(SE_SYSTEM);
+		titlePosY[1] += 2.0f;
 
-		//終了処理へ
-		g_CurrentSceneID = SCENE_ID_FIN_TITLE;
+		if (titlePosY[1] >= 200.0f)
+		{
+			titlePosY[1] = 200.0f;
+
+			FrameCount++;
+			{
+				if (FrameCount >= FRAME_RATE)
+				{
+					FrameCount = 0;
+					isReach[1] = true;
+				}
+			}
+		}
 	}
 
-	//矢印キーで状態変化
-	if (g_GameModeID != GAME_MODE_EASY) {
-		if (Input::Key::Push(KEY_INPUT_LEFT))
+	if (isReach[1])
+	{
+		//現れる
+		if (lighting)
+		{
+			fade[0] += 4;
+			fade[1] += 7;
+
+			if (fade[0] >= 255)
+			{
+				lighting = false;
+			}
+			if (fade[1] >= 255)
+			{
+				lighting = false;
+			}
+		}
+		//消える
+		else
+		{
+			fade[0] -= 4;
+			fade[1] -= 7;
+
+			if (fade[0] <= 0)
+			{
+				lighting = true;
+			}
+			if (fade[1] <= 0)
+			{
+				lighting = true;
+			}
+		}
+		//エンターキーで画面変化
+		if (Input::Key::Push(KEY_INPUT_RETURN))
 		{
 			//se
 			Sound::Se::Play(SE_SYSTEM);
-			if(g_GameModeID== GAME_MODE_HARD)
-				g_GameModeID= GAME_MODE_NORMAL;
-			else
-				g_GameModeID = GAME_MODE_EASY;
+
+			FrameCount++;
+
+			int loop = 0;
+
+			if (loop < 3)
+			{
+				if (FrameCount >= FRAME_RATE / 2)
+				{
+					FrameCount = 0;
+					loop++;
+
+					if (bright == 0)
+					{
+						bright = 122;
+					}
+					else if (bright == 122)
+					{
+						bright = 0;
+					}
+				}
+			}
+
+			if (loop == 3)
+			{
+				//終了処理へ
+				g_CurrentSceneID = SCENE_ID_FIN_TITLE;
+			}
+		}
+
+		//矢印キーで状態変化
+		if (g_GameModeID != GAME_MODE_EASY) {
+			if (Input::Key::Push(KEY_INPUT_LEFT))
+			{
+				//se
+				Sound::Se::Play(SE_SYSTEM);
+				if (g_GameModeID == GAME_MODE_HARD)
+					g_GameModeID = GAME_MODE_NORMAL;
+				else
+					g_GameModeID = GAME_MODE_EASY;
+			}
+		}
+		if (g_GameModeID != GAME_MODE_HARD) {
+			if (Input::Key::Push(KEY_INPUT_RIGHT))
+			{
+				//se
+				Sound::Se::Play(SE_SYSTEM);
+				if (g_GameModeID == GAME_MODE_EASY)
+					g_GameModeID = GAME_MODE_NORMAL;
+				else
+					g_GameModeID = GAME_MODE_HARD;
+			}
 		}
 	}
-	if (g_GameModeID != GAME_MODE_HARD) {
-		if (Input::Key::Push(KEY_INPUT_RIGHT))
-		{
-			//se
-			Sound::Se::Play(SE_SYSTEM);
-			if (g_GameModeID == GAME_MODE_EASY)
-				g_GameModeID = GAME_MODE_NORMAL;
-			else
-				g_GameModeID = GAME_MODE_HARD;
-		}
-	}
-	
 }
 
 
-void Title::Draw()//800 600
-{	
-	//タイトル下地
-	DrawGraph(0, 0, imageHandle[TITLE_TITLE], true);	
+void Title::Draw()//1280 720
+{
+	//背景
+	DrawGraph(0, 0, imageHandle[TITLE_BACK], true);
 
-	//左上
-	DrawRotaGraph(15, 30,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR1], true);
+	//タイトル
+	DrawRotaGraph(640, (int)titlePosY[0], 0.75f, 0.0f, imageHandle[TITLE_TITLE1], true);
+	DrawRotaGraph(640, (int)titlePosY[1], 0.75f, 0.0f, imageHandle[TITLE_TITLE2], true);
 
-	//左下
-	DrawRotaGraph(0, 450,0.5, (int)gearAngle*-1+0.4, imageHandle[TITLE_GEAR2], true);	
-	DrawRotaGraph(34, 552, 0.5, (int)gearAngle*-1+0.1, imageHandle[TITLE_GEAR2], true);
-	DrawRotaGraph(92, 479, 0.5, (int)gearAngle, imageHandle[TITLE_GEAR2], true);
+	if (isReach[1])
+	{
+		//難易度
+		SetDrawBlendMode(DX_BLENDMODE_ADD, bright);
+		DrawRotaGraph(640, 360, 0.75f, 0.0f, imageHandle[TITLE_EASY + g_GameModeID], true);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
 
-	//右下
-	DrawRotaGraph(760, 460,1.0f, (int)gearAngle*-1+0.5, imageHandle[TITLE_GEAR3], true);	
-	DrawRotaGraph(663, 576,1.0f, (int)gearAngle, imageHandle[TITLE_GEAR3], true);	
+	//柱
+	DrawGraph(0, 0, imageHandle[TITLE_PILLAR1], true);
+	DrawGraph(980, 0, imageHandle[TITLE_PILLAR2], true);
 
-	DrawRotaGraph(400, 440, 1.0f, 0.0f, imageHandle[TITLE_EASY+g_GameModeID], true);
+	if (isReach[1])
+	{
+		//矢印
+		if (g_GameModeID != GAME_MODE_NORMAL) {
+			//fadeで透明度変更
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade[1]);
+			DrawRotaGraph(500, 360, 0.75f, 0.0f, imageHandle[TITLE_ARROW2], true);
+			DrawRotaGraph(778, 360, 0.75f, 0.0f, imageHandle[TITLE_ARROW1], true);
+			//表示を元に戻す
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	//タイトル下の線 座標、左上座標、描画するサイズ、ハンドル、透明化、画像反転有無  421 43
-	DrawGraph(210, 310, imageHandle[TITLE_LINE], true);
-	//DrawRotaGraph(400, 380, 1.0f,0.0f,imageHandle[TITLE_GEARHARF], true);
+		}
+		else {
+			//fadeで透明度変更
+			SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade[1]);
+			DrawRotaGraph(500, 360, 0.75f, 0.0f, imageHandle[TITLE_ARROW2], true);
+			DrawRotaGraph(778, 360, 0.75f, 0.0f, imageHandle[TITLE_ARROW1], true);
+			//表示を元に戻す
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	//矢印
-	if (g_GameModeID != GAME_MODE_NORMAL) {
+		}
+
 		//fadeで透明度変更
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade[1]);
-		DrawRotaGraph(250, 440, 0.5, 0, imageHandle[TITLE_ARROW2], true);
-		DrawRotaGraph(540, 440, 0.5, 0.0f, imageHandle[TITLE_ARROW], true);
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade[0]);
+		DrawRotaGraph(640, 600, 0.75f, 0.0f, imageHandle[TITLE_START], true);	//入力待ち
 		//表示を元に戻す
 		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
 	}
-	else {
-		//fadeで透明度変更
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade[1]);
-		DrawRotaGraph(200, 440, 0.5, 0, imageHandle[TITLE_ARROW2], true);
-		DrawRotaGraph(590, 440, 0.5, 0.0f, imageHandle[TITLE_ARROW], true);
-		//表示を元に戻す
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-
-	}
-
-	//fadeで透明度変更
-	SetDrawBlendMode(DX_BLENDMODE_ALPHA, fade[0]);
-	DrawGraphF(300, 370, imageHandle[TITLE_START], true);	//入力待ち
-	//表示を元に戻す
-	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-
 }
 
 //終了処理
--- src/Scene/Title/Title.h
+++ src/Scene/Title/Title.h
@@ -3,19 +3,17 @@
 //画像の種類
 enum TITLE_IMAGE_NUM
 {
-	
-	TITLE_TITLE,	//タイトル
-	TITLE_START,	//入力待ち
-	TITLE_GEAR1,	//歯車
-	TITLE_GEAR2,
-	TITLE_GEAR3,
-	TITLE_GEARHARF,
-	TITLE_LINE,		//線
-	TITLE_EASY,		//easy
-	TITLE_NORMAL,	//normal
-	TITLE_HARD,		//hard
-	TITLE_ARROW,	//難易度選択用やじるし
-	TITLE_ARROW2,	//難易度選択用やじるし
+	TITLE_BACK,		//背景
+	TITLE_TITLE1,	//タイトル1
+	TITLE_TITLE2,	//タイトル2
+	TITLE_START,	//スタート
+	TITLE_EASY,		//イージー(4×4)
+	TITLE_NORMAL,	//ノーマル(5×5)
+	TITLE_HARD,		//ハード(6×6)
+	TITLE_PILLAR1,	//柱1
+	TITLE_PILLAR2,	//柱2
+	TITLE_ARROW1,	//矢印1
+	TITLE_ARROW2,	//矢印2
 
 	TITLE_IMAGE_MAX_NUM,	//画像数
 };
@@ -23,22 +21,17 @@ enum TITLE_IMAGE_NUM
 //画像パス
 const char TITLE_IMAGE_PATH[TITLE_IMAGE_MAX_NUM][255] =
 {
-	"data/title/title2.png",	//タイトル
-	"data/title/push.png",	//入力待ち
-	"data/title/gear1.png",	//歯車1
-	"data/title/gear2.png",	//歯車2
-	"data/title/gear3.png",	//歯車3
-	"data/title/gear2herf .png",	//歯車3
-	"data/title/sen.png",	//線
-	
-	"data/title/easy.png",	//easy
-	"data/title/normal.png",//normal
-	"data/title/hard.png",	//hard 
-	"data/title/sankaku.png",//三角
-	"data/title/sankaku02.png",//三角
-
-
-	//"data/title/start.png",	//入力待ち
+	"data/title/back.png",			//背景
+	"data/title/titletext1.png",	//タイトル1
+	"data/title/titletext2.png",	//タイトル2
+	"data/title/pressenter.png",	//スタート
+	"data/title/easy.png",			//イージー
+	"data/title/normal.png",		//ノーマル
+	"data/title/hard.png",			//ハード
+	"data/title/pillar.png",		//柱1
+	"data/title/pillar.png",		//柱2
+	"data/title/arrow1.png",		//矢印1
+	"data/title/arrow2.png",		//矢印2
 };
 
 class Title
@@ -46,18 +39,19 @@ class Title
 private:
 	int imageHandle[TITLE_IMAGE_MAX_NUM];	//画像ハンドル
 
-	
-	int fade[2];	//透明度：タイトル 入力待ち
+	int fade[2];	//透明度：スタート 矢印
+
+	int bright;		//明るさ：難易度
 
 	bool lighting;	//入力待ちの点滅
 
 	int progress;	//進行度
 
-	float gearAngle;	//歯車の回転
-	float lineX;		//タイトル下の線
-	float lineY;		//タイトル下の線
-	float lineW;		//タイトル下の線
-	float lineH;		//タイトル下の線
+	float titlePosY[2];	//タイトルのY座標
+
+	bool isReach[2];	//到達フラグ
+
+	int FrameCount;		//フレームカウント
 
 public:
 
